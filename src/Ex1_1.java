d658f496ccbc2e3454a8321775f7d36b4a96e6e8
Milian Wolff <mail@milianw.de>
2011-11-11 15:06:40 +0100

    let user pick first obj file in ex1_1

--- src/Ex1_1.java
+++ src/Ex1_1.java
@@ -7,6 +7,7 @@ import jv.project.PvDisplayIf;
 import jv.viewer.PvViewer;
 import jv.object.PsConfig;
 import jv.geom.PgPointSet;
+import jv.loader.PgFileDialog;
 import jv.loader.PjImportModel;
 
 /**
@@ -26,13 +27,20 @@ public class Ex1_1 {
 		// Get default display from viewer
 		PvDisplayIf disp = viewer.getDisplay();
 		
+		// get dir/first file
+		PgFileDialog dlg = new PgFileDialog(frame, "open geometry", 0);
+		dlg.setDirectory(PsConfig.getCodeBase() + "../proj1");
+		dlg.show();
+		if (!dlg.isFileSelected()) {
+			return;
+		}
+
 		// load file
 		PjImportModel importer = new PjImportModel();
-		String dataDir = PsConfig.getCodeBase() + "../proj1/";
-		File dir = new File(dataDir);
+		File dir = new File(new File(dlg.getFullFileName()).getParent());
 		String[] files = dir.list();
 		if (files == null) {
-			System.err.println("Could not open data dir: " + dataDir);
+			System.err.println("Could not open data dir: " + dir.getAbsolutePath());
 			System.err.println("see README for how to run this app");
 			return;
 		}
@@ -40,12 +48,13 @@ public class Ex1_1 {
 			if (!file.endsWith(".obj")) {
 				continue;
 			}
-			System.out.println("loading data from file: " + dataDir + file);
-			if (importer.load(dataDir + file)) {
+			file = dir.getAbsolutePath() + "/" + file;
+			System.out.println("loading data from file: " + file);
+			if (importer.load(file)) {
 				PgPointSet geometry = (PgPointSet)importer.getGeometry().clone();
 				System.out.println("euler characteristic for this file:" + geometry.getEulerCharacteristic());
 			} else {
-				System.err.println("PjModel.start(): failed loading geometry from file = " + dataDir + file);
+				System.err.println("PjModel.start(): failed loading geometry from file = " + file);
 			}
 		}
 
