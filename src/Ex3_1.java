4db26a3517cc239b75e29935fce12fe60b3d599a
Milian Wolff <mail@milianw.de>
2012-01-10 15:57:57 +0100

    start with third exercise, import code from programming template

---
+++ src/Ex3_1.java
@@ -0,0 +1,101 @@
+/*
+	Copyright 2012 Milian Wolff <mail@milianw.de>
+	
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License as
+	published by the Free Software Foundation; either version 2 of 
+	the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import java.awt.Color;
+import java.awt.Rectangle;
+
+import jv.geom.PgVectorField;
+import jv.project.PvCameraIf;
+import jv.project.PvGeometryListenerIf;
+import jv.vecmath.PdVector;
+import jvx.surface.PgDomain;
+import jvx.surface.PgDomainDescr;
+import jvx.vector.PwLIC;
+
+/**
+ * Solution to fourth exercise of the second project
+ * 
+ * @author		Milian Wolff
+ * @version		10.01.2012, 1.00 created
+ */
+public class Ex3_1 extends ProjectBase implements PvGeometryListenerIf
+{
+	private PwLIC m_lic;
+	private PgDomain m_domain;
+	private PgVectorField m_vec;
+
+	public static void main(String[] args)
+	{
+		new Ex3_1(args);
+	}
+	public Ex3_1(String[] args)
+	{
+		super(args, "SciVis - Project 3 - Exercise 1 - Milian Wolff");
+
+		// listener
+		m_disp.addGeometryListener(this);
+
+		m_domain = new PgDomain(2);
+		m_domain.setName("Domain for Vector Field");
+		m_domain.setDimOfElements(3);
+		
+		PgDomainDescr descr = m_domain.getDescr();
+		descr.setMaxSize(-10., -10., 10., 10.);
+		descr.setSize( -5., -5., 5., 5.);
+		descr.setDiscrBounds(2, 2, 50, 50);
+		descr.setDiscr(10, 10);
+		m_domain.compute();
+		
+		m_vec = new PgVectorField(3);
+		m_vec.setBasedOn(PgVectorField.VERTEX_BASED);
+		m_vec.setNumVectors(m_domain.getNumVertices());
+		m_vec.setGeometry(m_domain);
+		m_vec.setGlobalVectorColor(Color.BLACK);
+		PdVector.setConstant(m_vec.getVectors(), 1);
+		m_domain.addVectorField(m_vec);
+		
+		m_lic = new PwLIC();
+		m_lic.setGeometry(m_domain);
+		m_lic.setStandalone(false);
+		m_lic.setFast(true);
+		m_lic.setLICSize(50);
+
+		m_disp.selectCamera(PvCameraIf.CAMERA_ORTHO_XY);
+		m_disp.addGeometry(m_domain);
+		m_disp.update(m_domain);
+		m_disp.fit();
+
+//		updateVectorField();
+		
+		show();
+		m_frame.setBounds(new Rectangle(420, 5, 1024, 550));
+	}
+
+	/**
+	 * (Re)Compute vector field.
+	 */
+	public void updateVectorField() {
+		//compute ramdom vector field
+		for (PdVector vec : m_vec.getVectors()) {
+			vec.m_data[0] = 2*Math.random()-1;
+			vec.m_data[1] = 2*Math.random()-1;
+			vec.m_data[2] = 2*Math.random()-1;
+		}
+		m_vec.update(m_vec);
+		m_lic.startLIC();
+	}
+}
